a35fabc1dd2c838e7f7878dc0a906e9bac0e3b37
Sang <sangnds2001@gmail.com>
2023-11-05 21:41:10 +0700

    change condition gruad

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    int x;
+    switch(x){
+        case 1: printf("One");
+        case 2: printf("Two");
+        default:
+        printf("End."); 
+    }
     printf("Hello work");
     return 0;
 }
